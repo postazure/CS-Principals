f31e34c147ca4ca2afec7d575d34e2603decaa50
Max Gordon <gordonmaxc@gmail.com>
2015-01-06 17:17:28 -0800

    prev_node test

--- doubly-linked-list/spec/node_spec.rb
+++ doubly-linked-list/spec/node_spec.rb
@@ -6,11 +6,18 @@ describe Node do
   it "has a value" do
     expect(node.value).to eq 55
   end
+
   it "has a prev_node" do
     test_node = Node.new(37)
     node.next_node = test_node
     next_node_value = node.next_node.value
     expect(next_node_value).to eq 37
   end
-  it "has a next_node"
+
+  it "has a next_node" do
+    test_node = Node.new(28)
+    node.prev_node = test_node
+    prev_node_value = node.prev_node.value
+    expect(prev_node_value).to eq 28
+  end
 end
