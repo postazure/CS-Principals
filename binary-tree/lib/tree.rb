fd84c8a38e377af9372750d27781b1ef872570b1
Max Gordon <gordonmaxc@gmail.com>
2015-01-07 15:32:45 -0800

    binary tree

--- binary-tree/lib/tree.rb
+++ binary-tree/lib/tree.rb
@@ -7,8 +7,6 @@ class Tree
   def dfs(value, node = @root)
     return if node.nil?
     return node if node.value == value
-    return dfs(value, node.left) || dfs(value, node.right)
-    # return dfs(value, node.left) unless dfs(value, node.left).nil?
-    # return dfs(value, node.right) unless dfs(value, node.right).nil?
+    dfs(value, node.left) || dfs(value, node.right)
   end
 end
